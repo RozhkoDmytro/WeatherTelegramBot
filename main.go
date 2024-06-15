5d5d0f911e55d6668d6c57c004ee2cc3e7befc78
Cyber-Canum <CyberCanumVenaticorum@gmail.com>
2024-06-15 13:00:38 +0300

    add file structure add first version of readme.md

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	secretKey := os.Getenv("SECRET_KEY")
+	println(secretKey)
+}
