9a44006d941531f016da1c1dc817b4ba055e4455
Cyber-Canum <CyberCanumVenaticorum@gmail.com>
2024-06-18 18:20:14 +0300

    delete types.go

--- pkg/bot/bot.go
+++ pkg/bot/bot.go
@@ -9,10 +9,40 @@ import (
 	"net/url"
 	"strconv"
 
-	"projecttelegrambot/pkg/types"
-
 	"github.com/joho/godotenv"
 )
+
+type Update struct {
+	UpdateID int `json:"update_id"`
+	Message  struct {
+		MessageID int `json:"message_id"`
+		From      struct {
+			ID        int    `json:"id"`
+			IsBot     bool   `json:"is_bot"`
+			FirstName string `json:"first_name"`
+			Username  string `json:"username"`
+			Language  string `json:"language_code"`
+		} `json:"from"`
+		Chat struct {
+			ID        int64  `json:"id"`
+			FirstName string `json:"first_name"`
+			Username  string `json:"username"`
+			Type      string `json:"type"`
+		} `json:"chat"`
+		Date     int    `json:"date"`
+		Text     string `json:"text"`
+		Entities []struct {
+			Offset int    `json:"offset"`
+			Length int    `json:"length"`
+			Type   string `json:"type"`
+		} `json:"entities"`
+	} `json:"message"`
+}
+
+type GetUpdatesResponse struct {
+	OK     bool     `json:"ok"`
+	Result []Update `json:"result"`
+}
 
 const (
 	telegramAPI          = "https://api.telegram.org/bot"
@@ -44,7 +74,7 @@ func GetInfo(c string) string {
 	return result
 }
 
-func GetUpdates(token string, offset int) (*types.GetUpdatesResponse, error) {
+func GetUpdates(token string, offset int) (*GetUpdatesResponse, error) {
 	url := fmt.Sprintf("%s%s/getUpdates?offset=%d", telegramAPI, token, offset)
 	resp, err := http.Get(url)
 	if err != nil {
@@ -56,8 +86,8 @@ func GetUpdates(token string, offset int) (*types.GetUpdatesResponse, error) {
 }
 
 // parseTelegramRequest handles incoming update from the Telegram web hook
-func ParseTelegramRequest(r *http.Response) (*types.GetUpdatesResponse, error) {
-	var update types.GetUpdatesResponse
+func ParseTelegramRequest(r *http.Response) (*GetUpdatesResponse, error) {
+	var update GetUpdatesResponse
 	if err := json.NewDecoder(r.Body).Decode(&update); err != nil {
 		log.Printf("could not decode incoming update %s", err.Error())
 		return nil, err
