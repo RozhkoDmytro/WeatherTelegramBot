9b26631c3ce09b0691ad015f8a65085b5fa8dcde
Cyber-Canum <CyberCanumVenaticorum@gmail.com>
2024-07-17 16:10:46 +0300

    add simple test

---
+++ pkg/mongodb/mongodb_test.go
@@ -0,0 +1,43 @@
+package mongodb_test
+
+import (
+	"io"
+	"log/slog"
+	"os"
+	"testing"
+
+	"projecttelegrambot/pkg/mongodb"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewApiMongoDB(t *testing.T) {
+	// Create logger
+	logger, err := createLogger("app.log")
+	if err != nil {
+		panic(err)
+	}
+	_, err = mongodb.NewApiMongoDB("http://wrongURL", logger)
+
+	if err == nil {
+		assert.Equal(t, "True", "False", "Wrong url")
+	}
+}
+
+// Create logger and set fields
+func createLogger(NameLog string) (*slog.Logger, error) {
+	// Create logger
+	file, err := os.OpenFile(NameLog, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0o666)
+	if err != nil {
+		return nil, err
+	}
+
+	w := io.MultiWriter(os.Stderr, file)
+	handler := slog.NewJSONHandler(w, &slog.HandlerOptions{
+		AddSource: true,
+	})
+	logger := slog.New(handler)
+	slog.SetDefault(logger)
+
+	return logger, nil
+}
